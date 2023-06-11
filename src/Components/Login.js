5d423318fcc5cd4637e43eca386db685d01f57bf
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2023-06-12 00:27:05 +0530

    google login added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Dashboard from './Dashboard';
+import { GoogleLogin } from 'react-google-login';
+import { refreshTokenSetup } from './utils/refreshToken';
 import './Login.css';
+
+const clientId =
+  '49752167316-6h38q6il9k5f53mgneegnamlecfsfc6q.apps.googleusercontent.com';
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -25,6 +29,17 @@ const Login = () => {
     }
   };
 
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(`Logged in successfully welcome ${res.profileObj.name} . \n See console for full profile object.`);
+    refreshTokenSetup(res);
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(`Failed to login.`);
+  };
+
   return (
     <div className="container">
       <div className="black-portion">
@@ -38,7 +53,7 @@ const Login = () => {
         <div className="form-box">
           <form onSubmit={handleSubmit} className="form">
             <div className="form-group">
-              <label htmlFor="email">Email adress</label>
+              <label htmlFor="email">Email address</label>
               <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required />
             </div>
             <div className="form-group">
@@ -52,6 +67,15 @@ const Login = () => {
         <div className="register-link">
           <p>Don't have an account? <a href="/register">Register here</a></p>
         </div>
+        <GoogleLogin
+          clientId={clientId}
+          buttonText="Login"
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={'single_host_origin'}
+          style={{ marginTop: '100px' }}
+          isSignedIn={true}
+        />
       </div>
     </div>
   );
---
+++ src/Components/LoginHooks.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useGoogleLogin } from 'react-google-login';
+
+// refresh token
+import { refreshTokenSetup } from './utils/refreshToken';
+
+const clientId =
+  '763964992261-h1gmhf1p1h3hcbn1nfdj32o7tpimorrl.apps.googleusercontent.com';
+
+function LoginHooks() {
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+    );
+    refreshTokenSetup(res);
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login`
+    );
+  };
+
+  const { signIn } = useGoogleLogin({
+    onSuccess,
+    onFailure,
+    clientId,
+    isSignedIn: true,
+    accessType: 'offline',
+   
+  });
+
+  return (
+    <button onClick={signIn} className="button">
+      <img src="icons/google.svg" alt="google login" className="icon"></img>
+
+      <span className="buttonText">Sign in with Google</span>
+    </button>
+  );
+}
+
+export default LoginHooks;
